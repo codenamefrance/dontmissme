10dd1c0df7995bea36db787f116c22b978f4ab92
codenamefrance <francescopiocorrado@outlook.it>
2023-08-05 12:02:17 +0200

    First raw-but-working version.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # dontmissme
 Python-written software that tries to simplify student's life by implementing a very famous study technique called "Active Recall".
+
+To implement the spaced-repetition algorithm, SM2 has been implemented in this script, which has this kind of difficulty-rating:
+    5: perfect response.
+    4: correct response after a hesitation.
+    3: correct response recalled with serious difficulty.
+    2: incorrect response; where the correct one seemed easy to recall.
+    1: incorrect response; the correct one remembered.
+    0: complete blackout.
+    Easiness: The easiness factor, a multipler that affects the size of the interval, determine by the quality of the recall.
+    Interval: The gap/space between your next review.
+    Repetitions: The count of correct response (quality >= 3) you have in a row.
--- dontmissme.py
+++ dontmissme.py
@@ -1,6 +1,6 @@
 from tinydb import TinyDB, Query, where
 from datetime import *
-from supermemo2 import SMTwo, day_mon_year
+from supermemo2 import SMTwo
 
 DB_DEFAULT = 'database.js'
 
@@ -14,27 +14,6 @@ class Topic:
         self.next_date = (the followin day)
         self.rev_score = 0
         self.subject = subject
-    
-    def update_title(self, newtitle):
-        self.title = newtitle
-
-    def update_rev(self, date):
-        self.rev_score += 1
-        self.last_date = date
-'''
-
-'''
-The values are the:
-Quality: The quality of recalling the answer from a scale of 0 to 5.
-5: perfect response.
-4: correct response after a hesitation.
-3: correct response recalled with serious difficulty.
-2: incorrect response; where the correct one seemed easy to recall.
-1: incorrect response; the correct one remembered.
-0: complete blackout.
-Easiness: The easiness factor, a multipler that affects the size of the interval, determine by the quality of the recall.
-Interval: The gap/space between your next review.
-Repetitions: The count of correct response (quality >= 3) you have in a row.
 '''
 
 def db_init(db_name):
@@ -42,6 +21,11 @@ def db_init(db_name):
     return db
 
 ## def db_save(topic):
+
+def date_to_string(date):
+    date_string = str(date.year) + '-' + str(date.month) + '-' + str(date.day)
+
+    return date_string
 
 def add_topic(db):
     rev_score = 0
@@ -53,8 +37,8 @@ def add_topic(db):
     date_obj = date.today()
     revision_obj=SMTwo.first_review(3) #automatically sets today's date as add date
 
-    date_add = str(date_obj.day) + '-' + str(date_obj.month) + '-' + str(date_obj.year)
-    date_next = str(revision_obj.review_date.year)+ '-' + str(revision_obj.review_date.month)+ '-' + str(revision_obj.review_date.day)
+    date_add = date_to_string(date_obj)
+    date_next = date_to_string(revision_obj.review_date)
 
     subject = input("Subject's name: ")
     db_id = db.insert({'title':title, 'desc':desc, 'add_date':date_add, 'last_date':date_add,
@@ -63,35 +47,46 @@ def add_topic(db):
     
     return db_id
 
-def update_topic(db_obj, review_obj):
-    date_next = str(review_obj.review_date.year)+ '-' + str(review_obj.review_date.month)+ '-' + str(review_obj.review_date.day)
-    today_is_last = date.today()
-    last_date = str(today_is_last.year) + '-' + str(today_is_last.month) + '-' + str(today_is_last.day)
+# Search of topics to revise today
 
-    db_obj.update({'last_date':last_date})
-    db_obj.update({'next_date':date_next})
-    db_obj.update({'easiness':review_obj.easiness})
-    db_obj.update({'interval':review_obj.interval})
-    db_obj.update({'repetitions':review_obj.repetitions})
+def revise_today(db):
+    today = date.today()
+    today_date = date_to_string(today)
 
-    print("Updated!")
-    
-    return
+    args = db.search(where('next_date') == today_date)
+    if(len(args)>0):
+        return args
+    else:
+        return None
+
+# Simple function that applies the SM2 algorithm to a specified topic
 
 def revise_topic(db, topic_id, difficulty):
     topic_obj = db.get(doc_id=topic_id)
-    print(topic_obj)
     topic_easiness = topic_obj['easiness']
     topic_repetitions = topic_obj['repetitions']
     topic_interval = topic_obj['interval']
     review_obj = SMTwo(topic_easiness, topic_interval, topic_repetitions).review(difficulty)
-    update_topic(db, review_obj)
+    update_topic(db, review_obj, topic_id)
 
     return
 
-#def funzione che cerca gli argomenti da ripassare in giornata, ritorna l'id nel db del topic
+# Applying revise_topic changes to the actual db
 
-#def spaced(topic):
+def update_topic(db_obj, review_obj, topic_id):
+    date_next = date_to_string(review_obj.review_date)
+    today_is_last = date.today()
+    last_date = date_to_string(today_is_last)
+
+    db_obj.update({'last_date':last_date}, doc_ids=[topic_id])
+    db_obj.update({'next_date':date_next}, doc_ids=[topic_id])
+    db_obj.update({'easiness':review_obj.easiness}, doc_ids=[topic_id])
+    db_obj.update({'interval':review_obj.interval}, doc_ids=[topic_id])
+    db_obj.update({'repetitions':review_obj.repetitions}, doc_ids=[topic_id])
+
+    print("Updated!")
+    
+    return
 
     
 def main():
@@ -99,11 +94,31 @@ def main():
     all_subj = set()
     #topic_id = add_topic(db_main)
 
-    revise_topic(db_main, 1, 3)
-
-
-
-
+    topics_to_revise = revise_today(db_main)
+    if topics_to_revise == None:
+        print("Non ci sono argomenti da ripassare oggi.")
+    else:
+        print("Devi ripassare i seguenti argomenti: ")
+        i=1
+        for item in topics_to_revise:
+            print("%d - %s" %(i, item['title']))
+            i+=1
+    try:
+        user_input = int(input("\nWhich topic would you like to revise? "))
+    except:
+        print("Invalid input")
+        exit()
     
+    if user_input in range(1, i):
+        revise_topic(db_main, topics_to_revise[user_input-1].doc_id, 3)
+    else:
+        print("Invalid choice.")
+        exit()
+
+
+
+
+
+
 
 main()
